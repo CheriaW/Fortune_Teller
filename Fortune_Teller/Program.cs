01a3cb02058e036b5e5761326e07ae6f5772f2b6
CheriaW <cheria.white@yahoo.com>
2017-09-29 12:10:24 -0400

    Fortune Teller2

--- Fortune_Teller/Program.cs
+++ Fortune_Teller/Program.cs
@@ -10,6 +10,29 @@ namespace Fortune_Teller
     {
         static void Main(string[] args)
         {
+            //asking user for name
+            Console.WriteLine("Hello, soul. What is your name...?");
+            Console.ReadLine();
+            
+            Console.WriteLine("Hmmm... That sounds familiar. What's your last name?");
+            Console.ReadLine();
+
+            //asking user for age and telling them their fortune based on answer
+            Console.WriteLine("Ah, yes. I've been expecting you. Tell me... How old are you now?");
+            Console.ReadLine();
+
+            //is their age odd or even?
+            string userAge = string.Parse(userAge);
+            if (userAge = 0 || userAge = 2 );
+            
+            else
+            {
+                Console.ReadLine();
+            }
+
+
+                Console.WriteLine("Really? I have some interesting news for you then.");
+
 
         }
     }
