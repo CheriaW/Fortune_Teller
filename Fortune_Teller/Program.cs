8dfbbe2cefac80e0b91dc5c9c84c37d097b085bd
CheriaW <cheria.white@yahoo.com>
2017-10-01 21:10:26 -0400

    Fortune TellerAlmost

--- Fortune_Teller/Program.cs
+++ Fortune_Teller/Program.cs
@@ -11,98 +11,162 @@ namespace Fortune_Teller
         static void Main(string[] args)
         {
             //asking user for name
-            Console.WriteLine("Hello, soul. What is your name...?");
-            Console.ReadLine();
+            Console.WriteLine("Hey, there. I sense you came to me because you want to know your\n fortune... With my crystal ball I can foresee your future. Let's get started.\nWhat is your full name?");
+
+         
+            string userFirst = Console.ReadLine();
+            string userLast = Console.ReadLine();
+
+            Console.WriteLine(" ");
+
+            Console.WriteLine(" ");
+
+            //finding users birth month
+            Console.WriteLine("Interesting. Out of the 12 months in a year which number represents the\nmonth you were born in?");
+            string bankTotal;
+            int birthMonth = int.Parse(Console.ReadLine());
             
-            Console.WriteLine("Hmmm... That sounds familiar. What's your last name?");
-            Console.ReadLine();
 
-            //user birth month
-            Console.WriteLine("You may be the one I've been waiting on all this time. Your energy has\n been driving me crazy! Just to be sure... Tell me what month you were born in?");
-            Console.ReadLine();
+                if (birthMonth >= 1 && birthMonth <= 4)
+                {
+                bankTotal = "$300,000";
+                }
+                else if (birthMonth >= 5 && birthMonth <= 8)
+                {
+                bankTotal = "7 trillion dollars which was illegally obtained through a series of bank roberies";
+                } 
+                else if (birthMonth >= 9 && birthMonth <= 12)
+                {
+                bankTotal = "no outstanding loans, no children to suck up all\n your money, and 15 million dollars";
+                }
+                else
+                {
+                bankTotal = "$0.00";
+                }
+            Console.WriteLine(" ");
 
-            Console.WriteLine("Hmmm what number month is that again?");
-            Console.ReadLine();
+            Console.WriteLine(" ");
+
 
             ////is their age odd or even?
             ////COME BACK TO (look up moduls, dont use case)
-            //string userAge = Console.ReadLine();
             //asking user for age and telling them their fortune based on answer
-            Console.WriteLine("Ah, yes. That's right. I've been expecting you. Tell me... How old are you now?");
-            int even = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ah, yes. That's right. I've been expecting you. Tell me... How old are you?");
+            string yearsToRetirement;
+            int userAge = int.Parse(Console.ReadLine());
             
            
-            if (even % 2 == 0)
-            { 
-                Console.WriteLine("Hmmm... Not sure how you'll take this but you have to work for another 40 years. If it makes you feel better you'll\nbe working as an alpaca rancher until retirement!");
+            if (userAge % 2 == 0)
+            {
+                yearsToRetirement = "6";
             }
 
             else
             {
-                Console.WriteLine("Really? That's awesome! You'll retire in less than two years!");
+                yearsToRetirement = "18";
             }
-            
+            Console.WriteLine(" ");
 
             // finding out the number of user siblings and where users vacation home will be
             Console.WriteLine("How many siblings do you have?");
+            string sibNum; 
             int userSib = int.Parse(Console.ReadLine());
 
             if (userSib <= 0)
             {
-                Console.WriteLine("You'll invest in a vacation home in Fiji BUT it gets infested with rats. \n The local teens use it for parties and vandalize every inch of your property. \n Your home smells like poo and eventually gets torn down while you're away \n because the rancid odor takes over the town.\n The townsfolk gather around your burning vacation home and sing campfire songs in celebration.");
+                sibNum = "Fiji BUT it gets infested with rats. \n The local teens use it for parties and vandalize every inch of your property. \n Your home smells like poo and eventually gets torn down while you're away \n because the rancid odor takes over the town.\n The townsfolk gather around your burning vacation home and sing campfire songs in celebration.";
             }
 
 
             else if (userSib == 1)
 
             {
-                Console.WriteLine("You'll invest in a vacation home in South America where you live out your golden years!");
+                sibNum = "Paris";
             }
 
             else if (userSib == 2)
             {
-                Console.WriteLine("You buy a 10,000 sq ft vacation home in Peru and have your own alpaca farm.");
+                sibNum = "Peru and have your own alpaca farm.";
             }
 
             else if (userSib == 3)
 
             {
-                Console.WriteLine("In ten years you will have a vacation home in Tokyo that you and your significant other \n love with and turn into your permanant home!");
+                sibNum = "Tokyo";
             }
 
             else 
 
             {
-                Console.WriteLine("Retirement goes extremely well for you and you now own all of Hawaii which you \n use as a vacation home. \n *cough cough* \n selfishly uprooting all natives from their homes and forcing them to leave the island.");
+                sibNum = "Hawaii";
             }
+          
            
-            Console.WriteLine("Hold on.... I'm feeling something....");
-           
-            Console.WriteLine(". . .");
+            Console.WriteLine(" ");
             
-            Console.WriteLine(". . .");
+            Console.WriteLine(" ");
             
 
 
             //ROYGBIV fortunes
-            Console.WriteLine("Tell me something.... do you know what ROYGBIV is?");
-            int roygbiv = int.Parse(Console.ReadLine());
+            Console.WriteLine("This might be weird but a persons favorite color from the ROYGBIV spectrum\nreally helps me better understand their fortune. If you don't know\nwhat ROYGBIV IS, enter 'Help' and I'll tell you.");
+            string favColor = Console.ReadLine();
+            string roygbiv;
 
-            switch (roygbiv)
-
+            roygbiv = "";
+            if (favColor == "help")
             {
-                case 0:
-                    Console.WriteLine("Excellent! I need to know what your favorite ROYGBIV color is!");
-                    break;
-
-                case 1:
-                    Console.WriteLine("It's fine, I'll tell you. ROYGBIV is an acrynom that stands for the colors Red, Orange, Yellow, Green, Blue, Indigo, and Violet.");
-                    break;
+                Console.WriteLine("ROYGBIV is an acronym for the colors red, orange, yellow, blue, green, indigo, and violet. If you ever forget you can just think of the colors of the rainbow!");
+                Console.WriteLine("What is your favorite ROYGBIV color?");
             }
 
+            else if (favColor == "red")
+            {
+                roygbiv = "the rapid";
+            }
+            else if (favColor == "orange")
+            {
+                roygbiv = "on Usain Bolt's back";
+            }
+            else if (favColor == "yellow")
+            {
+                roygbiv = "on a massive army of ants";
+            }
+            else if (favColor == "green")
+            {
+                roygbiv = "in a brand new BMW";
+            }
+            else if (favColor == "blue")
+            {
+                roygbiv = "a shiny new Farari";
+            }
+            else if (favColor == "indigo")
+            {
+                roygbiv = "in your own private jet";
+            }
+            else if (favColor == "violet")
+            { 
+                roygbiv = "in the Batmobile";
+            }
+            else
+            {
+                roygbiv = "in a rainbow patteren Mystery Van";
+            }
+
+   Console.WriteLine(" ");
+            
+
+        //final fotrune telling
+            Console.WriteLine("Alright, I'm able to tell you your fortune... If you're not happy\n with it then oh well. This was a free reading.");
+            Console.WriteLine("My crystal ball sees that " + userFirst + userLast);
+            Console.WriteLine("will retire in " + yearsToRetirement + ("years with " + bankTotal));
+            Console.WriteLine("in the bank. \n You'll also have a vacation home in " + sibNum);
+            Console.WriteLine("Finally, you will ride " + roygbiv + " everywhere you go!");
+           
 
 
-
+      
+        
 
 
             }
